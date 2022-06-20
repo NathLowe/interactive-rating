1bd59b08e316d87a3ee991323210a0adb462bd08
NathLowe <nathanlowe04122001@gmail.com>
2022-06-20 16:34:25 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// Rating the article
+var lis = document.querySelectorAll('main #rate ul li');
+var changeSelected = function (node) { return function () {
+    console.log(node);
+    var previouslySelected = document.querySelector('main #rate ul li.selected');
+    if (previouslySelected) {
+        previouslySelected.classList.remove('selected');
+    }
+    node.classList.add('selected');
+}; };
+for (var index = 0; index < lis.length; index++) {
+    var li = lis[index];
+    li.addEventListener('click', changeSelected(li));
+}
+// Validating the rate
+var submit = document.querySelector('main #rate button');
+var validate = function (e) {
+    e.preventDefault;
+    var rateSelected = document.querySelector('main #rate ul li.selected');
+    if (rateSelected) {
+        var rate_1 = rateSelected.getAttribute('rate');
+        var rateSection_1 = document.querySelector('main section#rate');
+        var thankSection_1 = document.querySelector('main section#thank');
+        if (rateSection_1) {
+            rateSection_1.classList.remove('show');
+            rateSection_1.classList.add('hiding');
+            window.setTimeout(function () {
+                rateSection_1.classList.remove('hiding');
+                rateSection_1.classList.add('hide');
+                thankSection_1.classList.remove('hide');
+                thankSection_1.classList.add('show');
+                thankSection_1.querySelector('span.rate').innerHTML = rate_1;
+            }, 1400);
+        }
+    }
+    else {
+        window.alert('Please rate the article first.');
+    }
+};
+submit.addEventListener('click', validate);
---
+++ app.ts
@@ -0,0 +1,47 @@
+// Rating the article
+let lis = document.querySelectorAll('main #rate ul li')
+
+let changeSelected = (node:Element)=>()=>{
+    console.log(node)
+    let previouslySelected = document.querySelector('main #rate ul li.selected')
+    if(previouslySelected){
+        previouslySelected.classList.remove('selected')
+    }
+    node.classList.add('selected')
+}
+
+for (let index = 0; index < lis.length; index++) {
+    let li = lis[index];
+    li.addEventListener('click',changeSelected(li))
+}
+
+
+// Validating the rate
+let submit = document.querySelector('main #rate button')
+
+let validate = (e:Event)=>{
+    e.preventDefault
+    let rateSelected = document.querySelector('main #rate ul li.selected')
+    if(rateSelected){
+        let rate : string = rateSelected.getAttribute('rate')
+        let rateSection = document.querySelector('main section#rate')
+        let thankSection = document.querySelector('main section#thank')
+        if(rateSection){
+            rateSection.classList.remove('show')
+            rateSection.classList.add('hiding')
+            window.setTimeout(()=>{
+                rateSection.classList.remove('hiding')
+                rateSection.classList.add('hide')
+                
+                thankSection.classList.remove('hide')
+                thankSection.classList.add('show')
+                thankSection.querySelector('span.rate').innerHTML = rate
+            },1400)
+        }
+    }else{
+        window.alert('Please rate the article first.')
+    }
+}
+
+submit.addEventListener('click',validate)
+
